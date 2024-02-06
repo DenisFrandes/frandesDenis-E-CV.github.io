805c6765934f4c006eb3d8fb2f698e8c64f64c2d
DenisFrandes <151079326+DenisFrandes@users.noreply.github.com>
2024-02-06 13:05:27 +0200

    Add files via upload

---
+++ script-P.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menuLinks");
+    const icon=document.querySelector(".mobileIcon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+}
